efeb58f5e4b0160be7ff59426be54602221bc545
Mushinako <8977737+Mushinako@users.noreply.github.com>
2022-12-01 21:46:40 -0800

    Day 02 - Solution

--- day_02/analysis.md
+++ day_02/analysis.md
@@ -1,5 +1,11 @@
-# Day 02 (XXX)
+# Day 02 (Rock Paper Scissors)
 
 ## Part 1
 
+The scores for each of 3×3 scenarios are easy to figure out, and they can be
+hard coded into some kind of `switch` statement to assign a score to each
+scenario. Adding the scores up will give the result
+
 ## Part 2
+
+Same as part 1, although the scores for each scenario is potentially different
--- day_02/solution.py
+++ day_02/solution.py
@@ -3,12 +3,67 @@
 
 from __future__ import annotations
 
+from dataclasses import dataclass
+from enum import Enum
 from typing import TYPE_CHECKING
 
 from utils import SolutionAbstract
 
 if TYPE_CHECKING:
-    _Data = type(None)
+    from typing import Self
+
+    _Data = list["_Round"]
+
+
+class _RPS(Enum):
+    ROCK = "rock"
+    PAPER = "paper"
+    SCISSORS = "scissors"
+
+
+@dataclass(frozen=True)
+class _Round:
+    opponent: _RPS
+    you: str
+
+    @classmethod
+    def from_row(cls, opponent_str: str, you: str) -> Self:
+        opponent = {"A": _RPS.ROCK, "B": _RPS.PAPER, "C": _RPS.SCISSORS}[opponent_str]
+        return cls(opponent, you)
+
+    def get_score_1(self) -> int:
+        match self.you:
+            case "X":
+                return (
+                    1 + {_RPS.ROCK: 3, _RPS.PAPER: 0, _RPS.SCISSORS: 6}[self.opponent]
+                )
+            case "Y":
+                return (
+                    2 + {_RPS.ROCK: 6, _RPS.PAPER: 3, _RPS.SCISSORS: 0}[self.opponent]
+                )
+            case "Z":
+                return (
+                    3 + {_RPS.ROCK: 0, _RPS.PAPER: 6, _RPS.SCISSORS: 3}[self.opponent]
+                )
+            case _:
+                raise ValueError(f"Unknown action {self.you=}")
+
+    def get_score_2(self) -> int:
+        match self.you:
+            case "X":
+                return (
+                    0 + {_RPS.ROCK: 3, _RPS.PAPER: 1, _RPS.SCISSORS: 2}[self.opponent]
+                )
+            case "Y":
+                return (
+                    3 + {_RPS.ROCK: 1, _RPS.PAPER: 2, _RPS.SCISSORS: 3}[self.opponent]
+                )
+            case "Z":
+                return (
+                    6 + {_RPS.ROCK: 2, _RPS.PAPER: 3, _RPS.SCISSORS: 1}[self.opponent]
+                )
+            case _:
+                raise ValueError(f"Unknown action {self.you=}")
 
 
 class Solution(SolutionAbstract):
@@ -20,13 +75,16 @@ class Solution(SolutionAbstract):
         """
         Process day 02 data.
         """
+        return [_Round.from_row(*line.split()) for line in raw_data]
 
-    def part_1(self) -> ...:
+    def part_1(self) -> int:
         """
         Day 02 part 1 solution.
         """
+        return sum(round.get_score_1() for round in self.data)
 
-    def part_2(self) -> ...:
+    def part_2(self) -> int:
         """
         Day 02 part 2 solution.
         """
+        return sum(round.get_score_2() for round in self.data)
