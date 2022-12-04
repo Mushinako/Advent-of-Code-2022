1c6f65ff0921c886ef9d0a2f0c2a66cb3f50f5fd
Mushinako <8977737+Mushinako@users.noreply.github.com>
2022-12-03 22:54:26 -0800

    Day 03 - Solution

--- day_03/analysis.md
+++ day_03/analysis.md
@@ -1,5 +1,14 @@
-# Day 03 (XXX)
+# Day 03 (Rucksack Reorganization)
 
 ## Part 1
 
+Divide each line into 2 equal halves, and [`set.intersection`][1] takes care of
+finding the shared element. The score can be calculated from each letter's
+ASCII value
+
 ## Part 2
+
+Similar to part 1, dividing the lines into groups of 3 and `set.intersection`
+finds the common
+
+[1]: https://docs.python.org/3/library/stdtypes.html#frozenset.intersection
--- day_03/solution.py
+++ day_03/solution.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 from utils import SolutionAbstract
 
 if TYPE_CHECKING:
-    _Data = type(None)
+    _Data = list[str]
 
 
 class Solution(SolutionAbstract):
@@ -20,13 +20,43 @@ class Solution(SolutionAbstract):
         """
         Process day 03 data.
         """
+        return raw_data
 
-    def part_1(self) -> ...:
+    def part_1(self) -> int:
         """
         Day 03 part 1 solution.
         """
+        sum_ = 0
+        for rucksack in self.data:
+            item_count = len(rucksack)
+            if item_count % 2:
+                raise ValueError("Rucksack is not evenly sized")
+            half_length = item_count // 2
+            comp1 = rucksack[:half_length]
+            comp2 = rucksack[half_length:]
+            (common,) = set(comp1) & set(comp2)
+            sum_ += self._to_priority(common)
+        return sum_
 
-    def part_2(self) -> ...:
+    def part_2(self) -> int:
         """
         Day 03 part 2 solution.
         """
+        sum_ = 0
+        elf_count = len(self.data)
+        if elf_count % 3:
+            raise ValueError("Elf count is not divisible by 3")
+        for i in range(0, elf_count, 3):
+            elf1, elf2, elf3 = self.data[i : i + 3]
+            (common,) = set(elf1) & set(elf2) & set(elf3)
+            sum_ += self._to_priority(common)
+        return sum_
+
+    @staticmethod
+    def _to_priority(letter: str) -> int:
+        """"""
+        if "A" <= letter <= "Z":
+            return ord(letter) - 38
+        if "a" <= letter <= "z":
+            return ord(letter) - 96
+        raise ValueError(f"Unknown priority letter: {letter}")
