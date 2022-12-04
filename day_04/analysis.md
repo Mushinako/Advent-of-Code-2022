722315be2e47f002d3bf7982a248fc62f467c2e8
Mushinako <8977737+Mushinako@users.noreply.github.com>
2022-12-03 23:06:46 -0800

    Day 04 - Solution

---
+++ day_04/analysis.md
@@ -0,0 +1,12 @@
+# Day 04 (Camp Cleanup)
+
+## Part 1
+
+An elf's work includes the other's if their start is smaller than the other's
+start and their end is larger than the other's end. Count how many these happen
+would get the result
+
+## Part 2
+
+An elf's work overlaps the other's if their start or end is between the other's
+start and end. Count how many these happen would get the result
---
+++ day_04/solution.py
@@ -0,0 +1,64 @@
+# pyright: reportMissingTypeStubs=false
+""""""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from utils import SolutionAbstract
+
+if TYPE_CHECKING:
+    _Range = tuple[int, int]
+    _Data = list[tuple[_Range, _Range]]
+
+
+class Solution(SolutionAbstract):
+    day = 4
+    data: _Data
+
+    @staticmethod
+    def _process_data(raw_data: list[str]) -> _Data:
+        """
+        Process day 04 data.
+        """
+        data: _Data = []
+        for row in raw_data:
+            elf1_str, elf2_str = row.split(",")
+            elf1_start, elf1_end = map(int, elf1_str.split("-"))
+            elf2_start, elf2_end = map(int, elf2_str.split("-"))
+            data.append(((elf1_start, elf1_end), (elf2_start, elf2_end)))
+        return data
+
+    def part_1(self) -> int:
+        """
+        Day 04 part 1 solution.
+        """
+        count = 0
+        for (elf1_start, elf1_end), (elf2_start, elf2_end) in self.data:
+            if elf1_start <= elf2_start and elf2_end <= elf1_end:
+                count += 1
+                continue
+            if elf2_start <= elf1_start and elf1_end <= elf2_end:
+                count += 1
+                continue
+        return count
+
+    def part_2(self) -> int:
+        """
+        Day 04 part 2 solution.
+        """
+        count = 0
+        for (elf1_start, elf1_end), (elf2_start, elf2_end) in self.data:
+            if elf1_start <= elf2_start <= elf1_end:
+                count += 1
+                continue
+            if elf1_start <= elf2_end <= elf1_end:
+                count += 1
+                continue
+            if elf2_start <= elf1_start <= elf2_end:
+                count += 1
+                continue
+            if elf2_start <= elf1_end <= elf2_end:
+                count += 1
+                continue
+        return count
