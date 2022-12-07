3851a722002a36519b19985fdc5f866b60ad851e
Mushinako <8977737+Mushinako@users.noreply.github.com>
2022-12-06 21:47:19 -0800

    Day 07 - Solution

--- day_07/analysis.md
+++ day_07/analysis.md
@@ -1,5 +1,6 @@
-# Day 07 (XXX)
+# Day 07 (No Space Left On Device)
 
-## Part 1
+## Part 1 & 2
 
-## Part 2
+A familiar tree structure can be implemented as nodes in the most readable
+form. Traversing the tree as a generator is also fun
--- day_07/solution.py
+++ day_07/solution.py
@@ -8,7 +8,10 @@ from typing import TYPE_CHECKING
 from utils import SolutionAbstract
 
 if TYPE_CHECKING:
-    _Data = type(None)
+    from collections.abc import Generator
+    from typing import TypeAlias
+
+    _Data: TypeAlias = "_Directory"
 
 
 class Solution(SolutionAbstract):
@@ -20,13 +23,105 @@ class Solution(SolutionAbstract):
         """
         Process day 07 data.
         """
+        root = _Directory(name="/")
+        root.parent = root
+        curr_dir: _Directory = root
 
-    def part_1(self) -> ...:
+        for line in raw_data:
+            match line.split():
+                case ["$", "cd", path]:
+                    match path:
+                        case root.name:
+                            curr_dir = root
+                        case "..":
+                            parent = curr_dir.parent
+                            if parent is None:
+                                raise ValueError("No parent found")
+                            curr_dir = parent
+                        case _:
+                            curr_dir = curr_dir.children_map[path]
+                case ["$", "ls"]:
+                    pass
+                case ["dir", name]:
+                    curr_dir.add_children(_Directory(name=name))
+                case [size, name]:
+                    curr_dir.add_children(_File(name=name, size=int(size)))
+                case _:
+                    raise ValueError(f"Unknown line: {line}")
+
+        return root
+
+    def part_1(self) -> int:
         """
         Day 07 part 1 solution.
         """
+        return sum(
+            node.size
+            for node in self.data.traverse()
+            if isinstance(node, _Directory) and node.size <= 100000
+        )
 
-    def part_2(self) -> ...:
+    def part_2(self) -> int:
         """
         Day 07 part 2 solution.
         """
+        target = self.data.size - 40000000
+        curr_dir: _Directory = self.data
+        for child in self.data.traverse():
+            if not isinstance(child, _Directory):
+                continue
+            if child.size < target:
+                continue
+            if child.size < curr_dir.size:
+                curr_dir = child
+
+        return curr_dir.size
+
+
+class _Node:
+    name: str
+    parent: None | _Directory
+    size: int | property[int]
+
+    def __init__(self, *, name: str) -> None:
+        self.name = name
+        self.parent = None
+
+
+class _Directory(_Node):
+    children_map: dict[str, _Node]
+    _size: None | int = None
+
+    def __init__(self, *, name: str) -> None:
+        super().__init__(name=name)
+        self.children_map = {}
+
+    @property
+    def size(self) -> int:
+        if self._size is None:
+            self._size = sum(child.size for child in self.children_map.values())
+        return self._size
+
+    def add_children(self, *nodes: _Node) -> None:
+        for node in nodes:
+            name = node.name
+            if name in self.children_map:
+                raise ValueError(f"Existing file: {name}")
+            self.children_map[name] = node
+            node.parent = self
+        self.children_map |= {node.name: node for node in nodes}
+
+    def traverse(self) -> Generator[_Node, None, None]:
+        """"""
+        yield self
+        for child in self.children_map.values():
+            if isinstance(child, _Directory):
+                yield from child.traverse()
+            else:
+                yield child
+
+
+class _File(_Node):
+    def __init__(self, *, name: str, size: int) -> None:
+        super().__init__(name=name)
+        self.size = size
