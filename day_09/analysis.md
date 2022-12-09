9d41056742d8ac844f097b59ad7379ea7fd4a33a
Mushinako <8977737+Mushinako@users.noreply.github.com>
2022-12-08 21:52:27 -0800

    Day 09 - Solution

--- day_09/analysis.md
+++ day_09/analysis.md
@@ -1,5 +1,12 @@
-# Day 09 (XXX)
+# Day 09 (Rope Bridge)
 
-## Part 1
+## Part 1 & 2
 
-## Part 2
+I love to use complex numbers to represent 2D coordinates due to their ease in
+calculations. I esentially hard-coded all possible scenarios:
+
+* If previous segment is 0 or 1 steps away (including diagonal), then next
+  segment doesn't move
+* If previous segment is 2 steps away (including diagonal), then the next
+  segment moves based on previous segment's direction
+* If previous segment is 3 or more steps away, then something is wrong
--- day_09/solution.py
+++ day_09/solution.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 from utils import SolutionAbstract
 
 if TYPE_CHECKING:
-    _Data = type(None)
+    _Data = list[tuple[str, int]]
 
 
 class Solution(SolutionAbstract):
@@ -20,13 +20,98 @@ class Solution(SolutionAbstract):
         """
         Process day 09 data.
         """
+        data: _Data = []
+        for line in raw_data:
+            if not (row := line.strip()):
+                continue
+            direction, count = row.split()
+            data.append((direction, int(count)))
+        return data
 
-    def part_1(self) -> ...:
+    def part_1(self) -> int:
         """
         Day 09 part 1 solution.
         """
+        visited: set[complex] = {0 + 0j}
+        head = 0 + 0j
+        tail = 0 + 0j
+        for direction_str, count in self.data:
+            match direction_str:
+                case "U":
+                    direction = 0 + 1j
+                case "D":
+                    direction = 0 - 1j
+                case "L":
+                    direction = -1 + 0j
+                case "R":
+                    direction = 1 + 0j
+                case d:
+                    raise ValueError(f"Unknown direction {d}")
+            for _ in range(count):
+                head += direction
+                tail = self._pull(head, tail)
+                visited.add(tail)
 
-    def part_2(self) -> ...:
+        return len(visited)
+
+    def part_2(self) -> int:
         """
         Day 09 part 2 solution.
         """
+        visited: set[complex] = {0 + 0j}
+        segments = [0 + 0j] * 10
+        for direction_str, count in self.data:
+            match direction_str:
+                case "U":
+                    direction = 0 + 1j
+                case "D":
+                    direction = 0 - 1j
+                case "L":
+                    direction = -1 + 0j
+                case "R":
+                    direction = 1 + 0j
+                case d:
+                    raise ValueError(f"Unknown direction {d}")
+            for _ in range(count):
+                segments[0] += direction
+                for i in range(len(segments) - 1):
+                    segments[i + 1] = self._pull(segments[i], segments[i + 1])
+                visited.add(segments[-1])
+
+        return len(visited)
+
+    def _pull(self, head: complex, tail: complex) -> complex:
+        """"""
+        diff = head - tail
+        # Overlap or touching
+        if -1 <= diff.real <= 1 and -1 <= diff.imag <= 1:
+            return tail
+        # Horizontal changes
+        match diff.real:
+            # Left
+            case -1 | -2:
+                real_move = -1
+            # Same column
+            case 0:
+                real_move = 0
+            # Right
+            case 1 | 2:
+                real_move = 1
+            # Unknown
+            case _:
+                raise ValueError(f"Unknown diff {diff}")
+        # Vertical changes
+        match diff.imag:
+            # Bottom
+            case -1 | -2:
+                imag_move = -1
+            # Same row
+            case 0:
+                imag_move = 0
+            # Top
+            case 1 | 2:
+                imag_move = 1
+            # Unknown
+            case _:
+                raise ValueError(f"Unknown diff {diff}")
+        return tail + complex(real_move, imag_move)
